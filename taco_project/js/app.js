11a8df658298a470033e7debf25553871ece047a
KB-Z <zdonner179@gmail.com>
2019-10-19 19:37:08 -0400

    planning out Taco Project, move forward.

--- taco_project/js/app.js
+++ taco_project/js/app.js
@@ -1,1 +1,14 @@
-console.log($);
+$(() => {
+
+  $.ajax ({
+    url: 'http://taco-randomizer.herokuapp.com/random/'
+  }).then((data) => {
+    console.log(data);
+    $data = data;
+    $taco = $('<div>').addClass('tacoFun').text($data);
+    $('body').append($taco);
+  })
+
+
+
+})
