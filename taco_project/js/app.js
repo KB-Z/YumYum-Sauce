4514fc8d208288c0e062e0d2df5f5013900c48e6
KB-Z <zdonner179@gmail.com>
2019-10-20 12:48:50 -0400

    Able to query and retrieve data from db.

--- taco_project/js/app.js
+++ taco_project/js/app.js
@@ -1,23 +1,36 @@
 $(() => {
 
+  tacoTask = '';
 
-let tacoArray = [];
+  const summonTaco = (event) => {
+    $('section').empty();
+    tacoTask = $(event.target).attr('class');
+    console.log(tacoTask);
+    // tacoQuantity = $('#number').val();
+    // console.log(tacoQuantity);
 
-  $.ajax ({
-    url: 'http://taco-randomizer.herokuapp.com/random/'
-  }).then((data) => {tacoArray.push(data);
-    console.log(tacoArray);
-    tacoArray.map(() => {
-      console.log(tacoArray);
-    })
-    // $taco = $('<div>').addClass('tacoFun')
-    // $taco.html(
-    //   data
-    // );
+    $.ajax ({
+      url: `http://taco-randomizer.herokuapp.com/${tacoTask}/`
+    }).then((data) => {
+      console.log(data);
+      baseLayer = data.base_layer;
+      condiment = data.condiment;
+      mixIn = data.mixin;
+      seasoning = data.seasoning;
+      shell = data.shell;
+      $taco = $('<div>').addClass('taco');
+      $taco.html(
+        `<span>Base Layer: ${baseLayer}</span>
+        <span>Condiment: ${condiment}</span>
+        <span>Mix-in: ${mixIn}</span>
+        <span>Seasoning: ${seasoning}</span>
+        <span>Shell: ${shell}</span>
+        <button class='another'>Nother One!</button>`
+      );
+      $('section').append($taco);
 
-  });
-  // $('body').append($taco);
-
-
+    });
+  };
+  $('button').on('click', summonTaco);
 
 });
