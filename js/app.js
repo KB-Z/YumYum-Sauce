268b5ddee8078ecaadcbfb2c7bfc81793c3949bd
KB-Z <zdonner179@gmail.com>
2019-10-20 15:18:05 -0400

    basic sticky nav setup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 $(() => {
 
+
   tacoTask = '';
+  window.onscroll = function() {myFunction()};
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+
 
   const summonTaco = (event) => {
     tacoTask = $(event.target).attr('id');
