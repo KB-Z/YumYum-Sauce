c0f21632fd9ee20118458d7ad75ed8107adb1c4e
KB-Z <zdonner179@gmail.com>
2019-10-20 11:34:10 -0400

    changed db data to an array

--- taco_project/js/app.js
+++ taco_project/js/app.js
@@ -1,14 +1,23 @@
 $(() => {
 
+
+let tacoArray = [];
+
   $.ajax ({
     url: 'http://taco-randomizer.herokuapp.com/random/'
-  }).then((data) => {
-    console.log(data);
-    $data = data;
-    $taco = $('<div>').addClass('tacoFun').text($data);
-    $('body').append($taco);
-  })
+  }).then((data) => {tacoArray.push(data);
+    console.log(tacoArray);
+    tacoArray.map(() => {
+      console.log(tacoArray);
+    })
+    // $taco = $('<div>').addClass('tacoFun')
+    // $taco.html(
+    //   data
+    // );
+
+  });
+  // $('body').append($taco);
 
 
 
-})
+});
